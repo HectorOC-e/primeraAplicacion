ae50e51cf58a8a44bf6c847cca2499fa785c69eb
HectorOC-e <osoriohector89@gmail.com>
2022-12-27 21:37:49 -0600

    Agregar archivos de proyecto.

---
+++ primeraAplicacion/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primeraAplicacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 9;
+            Console.WriteLine($"Bienvenidos a C#{edad}");
+        }
+    }
+}
